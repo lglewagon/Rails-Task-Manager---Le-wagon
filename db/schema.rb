7ffb62289d69175ce6f9ece223ce163a3b4d3483
Louis Gesbert <louis.gesbert@hotmail.com>
2021-07-03 15:05:33 +0200

    App Rails Task Manager - Done - LG - Uniquement les obligatoires

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_090418) do
+ActiveRecord::Schema.define(version: 2021_07_03_091206) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
